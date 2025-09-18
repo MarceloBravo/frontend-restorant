f2fe4074e8f4264911b71f8ff7df2f5ec4b4ae4e
Marcelo Bravo <mabc@live.cl>
2025-09-18 20:02:05 -0300

    Se agregó la funcionalidad de monitoreo y validación de token en el hook useUser.js

--- src/Hooks/useUser.js
+++ src/Hooks/useUser.js
@@ -5,10 +5,12 @@ import { getLocalStorage } from '../shared/storage'
 import { logOut } from '../store/slices/loginSlices'
 import { useNavigate } from 'react-router-dom'
 import { clearLocalStorage } from '../shared/storage'
+import { isTokenExpired } from '../shared'
 
 export const useUser = () => {
     const accessToken = getLocalStorage('access')
     const refreshToken = getLocalStorage('refresh')
+    const interval = 5000
     const data = useSelector(state => state.login)
     const dispatch = useDispatch()
     const navigate = useNavigate()
@@ -19,14 +21,23 @@ export const useUser = () => {
         }
     }, [accessToken, refreshToken, dispatch])
 
+
     useEffect(() => {
-        if(data.isLogged === false){
-            dispatch(logOut())
-            clearLocalStorage()
-            navigate('/admin/login')
-        }
+        const intervalId = setInterval(() => {
+            console.log('Verificando token...'  )
+            if(!accessToken || isTokenExpired(accessToken)){
+                console.log('Token expirado')
+                dispatch(logOut())
+                clearLocalStorage()
+                navigate('/admin/login')
+                clearInterval(intervalId);
+            }
+        }, interval);
+
+        return () => clearInterval(intervalId);
         // eslint-disable-next-line
-    },[data])
+    },[]);
+        
 
     return data
 }
--- src/shared/validaciones.js
+++ src/shared/validaciones.js
@@ -2,3 +2,29 @@ export const validaEmail = (email) => {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
 }
+
+
+export const parseJwt = (token) => {
+  try {
+    const base64Url = token.split('.')[1]; // el payload es la segunda parte
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split('')
+        .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+        .join('')
+    );
+    return JSON.parse(jsonPayload);
+  } catch (e) {
+    console.error("Token inválido", e);
+    return null;
+  }
+}
+
+export const isTokenExpired = (token) => {
+  const payload = parseJwt(token);
+  if (!payload || !payload.exp) return true; // inválido o sin exp
+
+  const now = Math.floor(Date.now() / 1000); // segundos actuales
+  return payload.exp < now;
+}
